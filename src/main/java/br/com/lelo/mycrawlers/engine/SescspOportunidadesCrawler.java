dc31835cf679f4fb351967ef538ed05406cf7adb
Léo Gato <leo.costa@tegra.me>
2018-01-18 21:18:19 -0200

    Correção do xpath e tratamento do resultado para inputstream

--- src/main/java/br/com/lelo/mycrawlers/engine/SescspOportunidadesCrawler.java
+++ src/main/java/br/com/lelo/mycrawlers/engine/SescspOportunidadesCrawler.java
@@ -1,23 +1,28 @@
 package br.com.lelo.mycrawlers.engine;
 
-import static javax.xml.xpath.XPathConstants.STRING;
-
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.apache.xerces.dom.NodeImpl;
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.DomSerializer;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.xml.sax.InputSource;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import br.com.lelo.mycrawlers.exception.SescspInvalidXpahExpression;
 
@@ -30,40 +35,34 @@ public class SescspOportunidadesCrawler {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@PostConstruct
-	public void start() {
+	public void start() throws Exception {
 		logger.info("\n\n*****start*****");
-		try {
-			this.filter(this.getContent());
-		} catch (XPathExpressionException e) {
-			logger.error("Xpression invalida");
-		} catch (ClientProtocolException e) {
-			logger.error("url invalida ou indisponível");
-		} catch (SescspInvalidXpahExpression e) {
-			logger.error(e.getMessage());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		this.filter(this.getContent());
 		logger.info("*****finish*****\n\n");
 	}
 
-	private void filter(String resultStringContent) throws XPathExpressionException, SescspInvalidXpahExpression {
+	private void filter(InputStream inputStream)
+			throws XPathExpressionException, SescspInvalidXpahExpression, SAXException, IOException, ParserConfigurationException {
 		logger.info("Filtering with expression: " + parameters.getXpathExpression());
 
-		InputSource source = new InputSource(new StringReader(resultStringContent));
+		CleanerProperties cleanerProperties = new CleanerProperties();
+		cleanerProperties.setCharset("UTF-8");
+		TagNode tagNode = new HtmlCleaner(cleanerProperties).clean(inputStream);
+		Document dom = new DomSerializer(cleanerProperties).createDOM(tagNode);
+
 		XPath xPath = XPathFactory.newInstance().newXPath();
-		XPathExpression expression = xPath.compile(parameters.getXpathExpression());
-		String result = expression.evaluate(source, STRING).toString().trim();
-		if (result.isEmpty()) {
+		NodeImpl result = (NodeImpl) xPath.evaluate(parameters.getXpathExpression(), dom, XPathConstants.NODE);
+		if (result == null) {
 			throw new SescspInvalidXpahExpression();
 		}
-		logger.info(result);
+		logger.info(result.getNodeName());
 	}
 
-	private String getContent() throws ClientProtocolException, IOException {
+	private InputStream getContent() throws ClientProtocolException, IOException {
 		logger.info("Searching in: " + parameters.getUrl());
-		String resultStringContent = Request.Get(parameters.getUrl()).execute().returnContent().asString();
-		logger.info("Result length: " + resultStringContent.length());
-		return resultStringContent;
+		InputStream inputStream = Request.Get(parameters.getUrl()).execute().returnContent().asStream();
+		logger.info("Result length: " + inputStream);
+		return inputStream;
 	}
 
 }
